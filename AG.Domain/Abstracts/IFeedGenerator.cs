b7855b3987f8b9cf9c48f2d758fc90764c16712f
mphadeuspacy <mphakheswa@gmail.com>
2015-10-27 20:53:21 +0200

    Initial Commit

---
+++ AG.Domain/Abstracts/IFeedGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace AG.Domain.Abstracts
+{
+  public interface IFeedGenerator
+  {
+    /// <summary>
+    /// This generates the tweet feed output based on the user & tweet inputs.
+    /// </summary>
+    void SimulateFeed();
+  }
+}
---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using AG.Domain.Abstracts;
+using AG.Domain.Concretes;
+
+namespace ConsoleTestApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IFeedGenerator feedGenerator = new TweetFeedGenerator();
+      feedGenerator.SimulateFeed();      
+    }
+  }
+}
